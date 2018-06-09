f450a5cbe127868bd231ea90507d25924076d99a
Joel Gama <joelgama2015@gmail.com>
2018-06-09 16:47:45 +0100

    Resposta à query 6.

---
+++ proj-java/src/main/java/engine/QuerySix.java
@@ -0,0 +1,47 @@
+package engine;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Map.Entry;
+import java.util.Iterator;
+import java.util.TreeMap;
+import common.Pair;
+import li3.TADCommunity;
+
+public class QuerySix {
+
+	public static List<Long> answersmostvoted(TCDExample c, int N, LocalDate begin, LocalDate end) {
+		Long key;
+		List<Long> ids = new ArrayList();
+		TreeMap<Long,List<Post>> postsPorScore = new TreeMap<>();
+
+		for(Post p: c.getPostsBetween(begin, end)){
+			key = p.getScore();
+			if (postsPorScore.get(key) == null){
+				ArrayList<Post> posts = new ArrayList<>();
+            	posts.add(p.clone());
+            	postsPorScore.put(key, posts);
+        	} else {
+            	postsPorScore.get(key).add(p.clone());
+        	}
+    	}
+
+    	ids = idsPorScore(postsPorScore,N);
+
+		return ids;
+	}
+
+	public static List<Long> idsPorScore(TreeMap posts, int N){
+        List<Long> postsPorScore = new ArrayList();
+
+        Iterator ittwo = posts.keySet().iterator();
+        while (ittwo.hasNext()) {
+            	for (Post p : ((List<Post>)posts.get(ittwo.next()))) {
+                	if (postsPorScore.size() < N) {postsPorScore.add(p.getId());}
+            	}
+        }
+
+        return postsPorScore;
+    }
+}
